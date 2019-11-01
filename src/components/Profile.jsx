96e7cf9ce7665648346cf21cc3bbc37e20924e06
hunteman <pivovarov.d@mail.ru>
2019-11-01 23:31:58 +0700

    v-1

---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <main className="content">
+        <div classname="imgFrame">
+          <img className="cover" src="http://cdn.cnn.com/cnnnext/dam/assets/170407220916-04-iconic-mountains-matterhorn-restricted.jpg"/>
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          my posts
+          <div>
+            new post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+      </main>
+  );
+}
+
+export default Profile;
